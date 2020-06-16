0a2e7e13dbc2eb6e633dcad778cb68dc3ae37c6e
cdom <dominguerilla@gmail.com>
2020-06-16 06:08:54 -0400

    Create a 3D GameObject grid out of arranged tiles

--- Assets/Docs/How to make tiles.md
+++ Assets/Docs/How to make tiles.md
@@ -9,6 +9,7 @@
 			-> Pivot Object
 				-> Cube
 	- the whole tile prefab should be 10x10x10 units in size
-2. The outer gameobject should be tagged 'Tile' and have its Unity pivot (not to be confused with the Pivot Object!) be at the back-bottom-left corner if facing the tile prefab from the front
+2. The outer gameobject should be tagged 'Tile', be on a layer called 'Tile', and have its Unity pivot (not to be confused with the Pivot Object!) be at the back-bottom-left corner if facing the tile prefab from the front
+	- only the outer gameobject should be tagged 'Tile'/be on the layer 'Tile'! The children should not.
 3. The Pivot should be at the local position of (5,5,5), have the local rotation of (0,0,0), and at (1,1,1) scale
 	- the idea being that we can rotate the physical representation of the tile by rotating its Pivot gameobject, NOT the outer gameobject itself
---
+++ Assets/code/GridScanner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridScanner : MonoBehaviour
+{
+    private void Start()
+    {
+        int tileSize = 10;
+        CreateGrid(transform.position, new Vector3(4, 4, 4), tileSize);
+    }
+
+    public GameObject[][][] CreateGrid(Vector3 firstTileLocation, Vector3 dimensions, int tileSize)
+    {
+        GameObject[][][] grid = new GameObject[(int)dimensions.x][][];
+        Vector3 scanStartLocation = firstTileLocation + (Vector3.forward * 5f);
+        int layerMask = 1 << 8;
+
+        for (int x = 0; x < dimensions.x; x++)
+        {
+            grid[x] = new GameObject[(int)dimensions.y][];
+            for (int y = 0; y < dimensions.y; y++)
+            {
+                grid[x][y] = new GameObject[(int)dimensions.z];
+                for (int z = 0; z < dimensions.z; z++)
+                {
+                    Vector3 scanLocation = new Vector3(scanStartLocation.x + (tileSize * x), scanStartLocation.y + (tileSize * y), scanStartLocation.z + (tileSize * z));
+                    RaycastHit hit;
+                    if (Physics.Raycast(scanLocation, -Vector3.forward, out hit, 5f, layerMask, QueryTriggerInteraction.Collide))
+                    {
+                        Debug.Log($"Found Tile {hit.transform.name} at {x},{y},{z}!");
+                        grid[x][y][z] = hit.transform.gameObject;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+}
