6677df1dcdc895e228597f26bc17a331f8d008ef
cdom <dominguerilla@gmail.com>
2020-07-13 20:40:09 -0400

    Init tile adjacency

--- Assets/code/CubeTile.cs
+++ Assets/code/CubeTile.cs
@@ -2,6 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ADJACENT_DIRECTION
+{
+    FORWARD,
+    BACK,
+    LEFT,
+    RIGHT,
+    UP,
+    DOWN
+}
+
 public class CubeTile 
 {
     GameObject[][][] grid;
@@ -9,20 +19,6 @@ public class CubeTile
     public int tileIndex { get; private set; }
     public int gridHashCode { get; private set; }
     public Vector3 sampleCoordinates { get; private set; }
-
-    /// <summary>
-    /// Creates a CubeTile.
-    /// </summary>
-    /// <param name="grid">The grid this tile will be sampled from.</param>
-    /// <param name="index">The tile index identifying this CubeTile. Should be unique among all CubeTiles.</param>
-    /// <param name="hashCode">The hashed names of all the modules contained in this tile. Should be unique among all CubeTiles.</param>
-    public CubeTile(GameObject[][][] grid, int index, int hashCode)
-    {
-        this.grid = grid;
-        this.dimension = grid[0].Length;
-        this.tileIndex = index;
-        this.gridHashCode = hashCode;
-    }
 
     /// <summary>
     /// Creates a CubeTile.
@@ -52,6 +48,52 @@ public class CubeTile
             return module;
         }
         return null;
+    }
+
+    public bool isCompatible(CubeTile other, ADJACENT_DIRECTION direction)
+    {
+        if (other.dimension != this.dimension)
+        {
+            throw new System.Exception("CubeTiles do not match in dimensions!");
+        }
+
+        //TODO: double-check these.
+        switch (direction)
+        {
+            case ADJACENT_DIRECTION.FORWARD:
+                /*
+                 compare all of this.grid[1...dimension][0...dimension][0...dimension]
+                 with other.grid[0...dimension-1][0...dimension][0...dimension]
+                 */
+            case ADJACENT_DIRECTION.BACK:
+                /*
+                 * compare all of this.grid[0...dimension-1][0...dimension][0...dimension]
+                 * with other.grid[1...dimension][0...dimension][0...dimension]
+                 */
+            case ADJACENT_DIRECTION.LEFT:
+                /*
+                 * compare all of this.grid[0...dimension][0...dimension][0...dimension-1]
+                 * with other.grid[0...dimension][0...dimension][1...dimension]
+                 */
+            case ADJACENT_DIRECTION.RIGHT:
+                /*
+                 * compare all of this.grid[0...dimension][0...dimension][1...dimension]
+                 * with other.grid[0...dimension][0...dimension][0...dimension-1]
+                 */
+            case ADJACENT_DIRECTION.UP:
+                /*
+                 * compare all of this.grid[0...dimension][1...dimension][0...dimension]
+                 * with other.grid[0...dimension][0...dimension-1][0...dimension]
+                 */
+            case ADJACENT_DIRECTION.DOWN:
+                /*
+                 * compare all of this.grid[0...dimension][0..dimension-1][0...dimension]
+                 * with other.grid[0...dimension][1...dimension][0...dimension]
+                 */
+                break;
+            default:
+                throw new System.Exception("Invalid adjacent direction!");
+        }
     }
 }
 
